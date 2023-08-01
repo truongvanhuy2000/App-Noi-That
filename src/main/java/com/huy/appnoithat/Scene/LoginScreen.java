9d9617792d4f7401d5911f3956af2ee809fb8db2
huy <truongvanhuy2000@gmail.com>
2023-08-02 01:04:14 +0700

    add login logic

--- src/main/java/com/huy/appnoithat/Scene/LoginScreen.java
+++ src/main/java/com/huy/appnoithat/Scene/LoginScreen.java
@@ -1,4 +1,21 @@
 package com.huy.appnoithat.Scene;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import lombok.Getter;
+
+import java.io.IOException;
+
+@Getter
 public class LoginScreen {
+    private final Scene loginScene;
+    public LoginScreen() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LoginLayout.fxml"));
+        try {
+            loginScene = new Scene(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
