09b1b221bc016329d69fd66537be45d6fe1764b5
huy <truongvanhuy2000@gmail.com>
2023-08-01 11:27:51 +0700

    fixed bug

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,14 @@
 module com.huy.appnoithat {
     requires javafx.controls;
     requires javafx.fxml;
-    requires poi;
-    requires poi-ooxml;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires lombok;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
 
     opens com.huy.appnoithat to javafx.fxml;
     exports com.huy.appnoithat;
