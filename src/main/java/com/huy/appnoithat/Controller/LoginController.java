8adc65659ba3125cf8675b014cec6cfad1553624
huy <truongvanhuy2000@gmail.com>
2023-08-02 23:59:44 +0700

    add lua chon noi that controller

--- src/main/java/com/huy/appnoithat/Controller/LoginController.java
+++ src/main/java/com/huy/appnoithat/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.huy.appnoithat.Controller;
 
+import com.huy.appnoithat.Service.Login.LoginService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -10,7 +11,14 @@ public class LoginController {
     TextField usernameTextField;
     @FXML
     PasswordField passwordField;
+    private LoginService loginService;
+    public LoginController() {
+        loginService = new LoginService();
+    }
     public void login(ActionEvent actionEvent) {
+        if (!loginService.Authorization(usernameTextField.getText(), passwordField.getText())) {
+            return;
+        }
 
     }
 }
--- src/main/java/com/huy/appnoithat/Controller/LuaChonNoiThatController.java
+++ src/main/java/com/huy/appnoithat/Controller/LuaChonNoiThatController.java
@@ -1,8 +1,46 @@
 package com.huy.appnoithat.Controller;
 
 import com.huy.appnoithat.Service.LuaChonNoiThat.LuaChonNoiThatService;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 
 public class LuaChonNoiThatController {
+    @FXML
+    private TableColumn<?, ?> Cao;
+
+    @FXML
+    private TableColumn<?, ?> Dai;
+
+    @FXML
+    private TableColumn<?, ?> DonGia;
+
+    @FXML
+    private TableColumn<?, ?> DonVi;
+
+    @FXML
+    private TableColumn<?, ?> HangMuc;
+
+    @FXML
+    private TableColumn<?, ?> NoiThat;
+
+    @FXML
+    private TableColumn<?, ?> PhongCach;
+
+    @FXML
+    private TableColumn<?, ?> Rong;
+
+    @FXML
+    private TableView<?> TableNoiThat;
+
+    @FXML
+    private TableColumn<?, ?> ThanhTien;
+
+    @FXML
+    private TableColumn<?, ?> VatLieu;
+
+    @FXML
+    private TableColumn<?, ?> id;
+
     LuaChonNoiThatService luaChonNoiThatService;
     public LuaChonNoiThatController() {
         luaChonNoiThatService = new LuaChonNoiThatService();
