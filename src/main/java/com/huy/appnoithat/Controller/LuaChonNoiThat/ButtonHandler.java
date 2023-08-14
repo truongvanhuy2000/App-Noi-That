5a566b1d17c56c5c19cc55c9012080e779a2d3b3
huy <truongvanhuy2000@gmail.com>
2023-08-15 01:00:09 +0700

    bug at file ButtonHandler at line 28

--- src/main/java/com/huy/appnoithat/Controller/LuaChonNoiThat/ButtonHandler.java
+++ src/main/java/com/huy/appnoithat/Controller/LuaChonNoiThat/ButtonHandler.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableView;
+import org.apache.poi.xdgf.util.Util;
 
 public class ButtonHandler {
     private TreeTableView<BangNoiThat> TableNoiThat;
@@ -14,42 +15,96 @@ public class ButtonHandler {
         this.TableNoiThat = tableNoiThat;
     }
     public void addNewLine(ActionEvent event) {
+//        if (TableNoiThat.getSelectionModel().getSelectedItems().isEmpty()){
+        TableNoiThat.getRoot().getChildren().add(new TreeItem<>(new BangNoiThat(
+                "A", 0f, 0f, 0f, 0L,
+                "", "", "", 0L, 0f)));
+        return;
+//        }
+//        if (isReachedLimit(TableNoiThat.getRoot())){
+//            ErrorUtils.throwErrorSignal("Đã đạt giới hạn số lượng 30");
+//            return;
+//        }
+//        TreeItem<BangNoiThat> currentSelectedItem = TableNoiThat.getSelectionModel().getSelectedItem();
+//        TreeItem<BangNoiThat> parent = currentSelectedItem.getParent();
+//
+//        int indexOfCurrentItem = parent.getChildren().indexOf(currentSelectedItem);
+//        parent.getChildren().add(indexOfCurrentItem + 1, new TreeItem<>(new BangNoiThat(
+//                "A", 0f, 0f, 0f, 0L,
+//                "", "", "", 0L, 0f)));
+//        parent.setExpanded(true);
+    }
+
+//    public void continuousLineAdd(ActionEvent event) {
+//        if (TableNoiThat.getSelectionModel().getSelectedItems().isEmpty()){
+//            return;
+//        }
+//        TreeItem<BangNoiThat> currentSelectedItem = TableNoiThat.getSelectionModel().getSelectedItem();
+//        TreeItem<BangNoiThat> parent = currentSelectedItem.getParent();
+//        String stt = parent.getChildren().get(parent.getChildren().size() - 1).getValue().getSTT().getValue();
+//        String nextStt = findTheNextStt(stt);
+//        if (isReachedLimit(parent)) {
+//            ErrorUtils.throwErrorSignal("Đã đạt giới hạn số lượng 30");
+//            return;
+//        }
+//        parent.getChildren().add(new TreeItem<>(new BangNoiThat(
+//                nextStt, 0f, 0f, 0f, 0L,
+//                "", "", "", 0L, 0f)));
+//    }
+    public void continuousLineAdd(ActionEvent event){
         if (TableNoiThat.getSelectionModel().getSelectedItems().isEmpty()){
-            TableNoiThat.getRoot().getChildren().add(new TreeItem<>(new BangNoiThat(
-                    "0", 0f, 0f, 0f, 0L,
-                    "", "", "", 0L, 0f)));
-            return;
-        }
-        if (isReachedLimit(TableNoiThat.getRoot())){
-            ErrorUtils.throwErrorSignal("Đã đạt giới hạn số lượng 30");
             return;
         }
         TreeItem<BangNoiThat> currentSelectedItem = TableNoiThat.getSelectionModel().getSelectedItem();
-        TreeItem<BangNoiThat> parent = currentSelectedItem.getParent();
-
-        int indexOfCurrentItem = parent.getChildren().indexOf(currentSelectedItem);
-        parent.getChildren().add(indexOfCurrentItem + 1, new TreeItem<>(new BangNoiThat(
-                "0", 0f, 0f, 0f, 0L,
-                "", "", "", 0L, 0f)));
-        parent.setExpanded(true);
-    }
-
-    public void continuousLineAdd(ActionEvent event) {
-        if (TableNoiThat.getSelectionModel().getSelectedItems().isEmpty()){
+        String currentItemStt = currentSelectedItem.getValue().getSTT().getValue();
+        if (Utils.RomanNumber.isRoman(currentItemStt)){
+            if (!isReachedLimit(currentSelectedItem)) {
+                continuousLineAddForRomanStt(currentSelectedItem);
+            }
             return;
         }
-        TreeItem<BangNoiThat> currentSelectedItem = TableNoiThat.getSelectionModel().getSelectedItem();
-        TreeItem<BangNoiThat> parent = currentSelectedItem.getParent();
-        String stt = parent.getChildren().get(parent.getChildren().size() - 1).getValue().getSTT().getValue();
-        String nextStt = findTheNextStt(stt);
-        if (isReachedLimit(parent)) {
-            ErrorUtils.throwErrorSignal("Đã đạt giới hạn số lượng 30");
+        if (Utils.isAlpha(currentItemStt)){
+            if (!isReachedLimit(currentSelectedItem)) {
+                continuousLineAddForAlphaStt(currentSelectedItem);
+            }
             return;
         }
-        parent.getChildren().add(new TreeItem<>(new BangNoiThat(
-                nextStt, 0f, 0f, 0f, 0L,
-                "", "", "", 0L, 0f)));
+        if (Utils.isNumeric(currentItemStt)){
+            if (!isReachedLimit(currentSelectedItem.getParent())) {
+                continuousLineAddForNumericStt(currentSelectedItem);
+            }
+        }
+//        TreeItem<BangNoiThat> parent = currentSelectedItem.getParent();
+
     }
+
+    private void continuousLineAddForNumericStt(TreeItem<BangNoiThat> currentSelectedItem) {
+        TreeItem<BangNoiThat> parent = currentSelectedItem.getParent();
+        TreeItem<BangNoiThat> tempNewItem = new TreeItem<>(new BangNoiThat("0", 0f, 0f, 0f, 0L,
+                "", "", "", 0L, 0f));
+
+        String nextStt = findTheNextStt(currentSelectedItem.getValue().getSTT().getValue());
+        tempNewItem.getValue().getSTT().setValue(nextStt);
+        parent.getChildren().add(tempNewItem);
+        TableNoiThat.getSelectionModel().select(tempNewItem);
+    }
+
+    private void continuousLineAddForAlphaStt(TreeItem<BangNoiThat> currentSelectedItem) {
+        TreeItem<BangNoiThat> tempNewItem = new TreeItem<>(new BangNoiThat("I", 0f, 0f, 0f, 0L,
+                "", "", "", 0L, 0f));
+        currentSelectedItem.getChildren().add(tempNewItem);
+        currentSelectedItem.setExpanded(true);
+        TableNoiThat.getSelectionModel().select(tempNewItem);
+    }
+
+    private void continuousLineAddForRomanStt(TreeItem<BangNoiThat> currentSelectedItem) {
+        TreeItem<BangNoiThat> tempNewItem = new TreeItem<>(new BangNoiThat("0", 0f, 0f, 0f, 0L,
+                "", "", "", 0L, 0f));
+        currentSelectedItem.getChildren().add(tempNewItem);
+        currentSelectedItem.setExpanded(true);
+        TableNoiThat.getSelectionModel().select(tempNewItem);
+    }
+
     public static String findTheNextStt(String stt) {
         if (Utils.RomanNumber.isRoman(stt)){
             int nextStt = Utils.RomanNumber.romanToInt(stt) + 1;
@@ -66,7 +121,11 @@ public class ButtonHandler {
     }
 
     public boolean isReachedLimit(TreeItem<BangNoiThat> root){
-        return root.getChildren().size() >= 30;
+        if (root.getChildren().size() >= 30){
+            ErrorUtils.throwErrorSignal("Đã đạt giới hạn số lượng 30");
+            return true;
+        }
+        return false;
     }
 
     public void onDeleteLine(ActionEvent event) {
