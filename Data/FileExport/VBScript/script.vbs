88cb1c9a0ac8e92d5ddcd9f1143a0a09ad6f90b5
huy <truongvanhuy2000@gmail.com>
2023-11-11 19:54:38 +0700

    pdf

--- Data/FileExport/VBScript/script.vbs
+++ Data/FileExport/VBScript/script.vbs
@@ -1,14 +1,36 @@
 Option Explicit
-Dim objExcel, strExcelPath, objSheet
+On Error Resume Next
 
-strExcelPath = "XL_FILE"
+Dim objExcel, objWorkbook, objSheet, pdfPath
 
+' Path to the Excel file (.xlsx)
+Dim excelFilePath
+excelFilePath = "EXEL_PATH"
 
+' Output PDF path
+pdfPath = "PDF_PATH"
+
+' Create Excel application object
 Set objExcel = CreateObject("Excel.Application")
-objExcel.WorkBooks.Open strExcelPath
-Set objSheet = objExcel.ActiveWorkbook.Worksheets(1)
+If Err.Number <> 0 Then
+    WScript.Echo "Error creating Excel object: " & Err.Description
+    WScript.Quit
+End If
 
-objSheet.ExportAsFixedFormat 0, "PDF_FILE",0, 1, 0, , , 0
+' Open Excel workbook
+Set objWorkbook = objExcel.Workbooks.Open(excelFilePath)
+If Err.Number <> 0 Then
+    WScript.Echo "Error opening Excel workbook: " & Err.Description
+    objExcel.Quit
+    WScript.Quit
+End If
 
-objExcel.ActiveWorkbook.Close
-objExcel.Application.Quit
+' Access the first worksheet
+Set objSheet = objWorkbook.Worksheets(1)
+
+' Export the worksheet as PDF
+objSheet.ExportAsFixedFormat 0, pdfPath, 1, 0, 0
+
+' Close the workbook and quit Excel
+objWorkbook.Close
+objExcel.Quit
--- src/main/java/com/huy/appnoithat/Service/FileExport/Operation/PDF/ExportPDF.java
+++ src/main/java/com/huy/appnoithat/Service/FileExport/Operation/PDF/ExportPDF.java
@@ -48,41 +48,40 @@ public class ExportPDF implements ExportFile {
     public DataPackage importData(File importDirectory) {
         return null;
     }
-    private void convertXLStoPDF(String directory){
+    public void convertXLStoPDF(String directory){
         try {
             //read all the lines of the .vbs script into memory as a list
             //here we pull from the resources of a Gradle build, where the vbs script is stored
+            Path tempScript = Files.createTempFile("script", ".vbs");
             List<String> script = Files.readAllLines(Paths.get(Config.FILE_EXPORT.VBS_SCRIPT_DIRECTORY));
 
-            // append test.xlsm for file name. savePath was passed to this function
-            String templateFile = directory;
-//            templateFile = templateFile.replace("\\", "\\\\");
-            String pdfFile = outputFile.getAbsolutePath();
-//            pdfFile = pdfFile.replace("\\", "\\\\");
-            System.out.println("templateFile is: " + templateFile);
-            System.out.println("pdfFile is: " + pdfFile);
-
-            //replace the placeholders in the vbs script with the chosen file paths
-            for (int i = 0; i < script.size(); i++) {
-                script.set(i, script.get(i).replaceAll("XL_FILE", templateFile));
-                script.set(i, script.get(i).replaceAll("PDF_FILE", pdfFile));
-                System.out.println("Line " + i + " is: " + script.get(i));
-            }
-
+//            // append test.xlsm for file name. savePath was passed to this function
+//            String templateFile = directory;
+////            templateFile = templateFile.replace("\\", "\\\\");
+//            String pdfFile = outputFile.getAbsolutePath();
+////            pdfFile = pdfFile.replace("\\", "\\\\");
+//            System.out.println("templateFile is: " + templateFile);
+//            System.out.println("pdfFile is: " + pdfFile);
+//
+//            //replace the placeholders in the vbs script with the chosen file paths
+            script = script.stream().map(line -> {
+                if (line.contains("EXEL_PATH")) {
+                    line = line.replace("EXEL_PATH", directory);
+                }
+                if (line.contains("PDF_PATH")) {
+                    line = line.replace("PDF_PATH", outputFile.getAbsolutePath());
+                }
+                return line;
+            }).toList();
+            Files.write(tempScript, script);
             //create a processBuilder for starting an operating system process
-            ProcessBuilder pb = new ProcessBuilder("wscript", script.toString());
+            ProcessBuilder pb = new ProcessBuilder("wscript", tempScript.toString());
 
             //start the process on the operating system
             Process process = pb.start();
 
             //tell the process how long to wait for timeout
             boolean success = process.waitFor(2, TimeUnit.MINUTES);
-            if(!success) {
-                System.out.println("Error: Could not print PDF within ");
-            } else {
-                System.out.println("Process to run visual basic script for pdf conversion succeeded.");
-            }
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
