6ef41bfaf9f128039b4740ad71fbcb6322e4ddb7
huy <truongvanhuy2000@gmail.com>
2024-09-13 00:05:17 +0700

    new update

--- src/test/java/com/huy/appnoithat/Controller/LuaChonNoiThat/Command/implementation/EditCommitThanhTienCommandTest.java
+++ src/test/java/com/huy/appnoithat/Controller/LuaChonNoiThat/Command/implementation/EditCommitThanhTienCommandTest.java
@@ -21,34 +21,3 @@ import org.testfx.framework.junit5.Start;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-
-@ExtendWith(ApplicationExtension.class)
-class EditCommitThanhTienCommandTest {
-
-
-    @Start
-    private void start(Stage stage) {
-
-    }
-
-    @Test
-    void execute() {
-        BangNoiThat testData = BangNoiThat.builder()
-                .id("1").thanhTien(100L).cao(100.0)
-                .dai(100.0).rong(100.0).donVi("test")
-                .donGia(100L).hangMuc("test").vatLieu("test").khoiLuong(100.0)
-                .build();
-        long expectedThanhTien = 999L;
-
-        Mockito.mockStatic(TableCalculationUtils.class);
-        when(event.getRowValue().getValue()).thenReturn(testData);
-        when(event.getNewValue()).thenReturn(expectedThanhTien);
-        command.execute();
-
-        Assertions.assertEquals(testData.getThanhTien(), expectedThanhTien);
-    }
-
-    @Test
-    void undo() {
-    }
-}
